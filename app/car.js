b9d84189931a32aa418ae3b19b8cee64f689f737
seyikeye <aromokeyes2@gmail.com>
2016-11-28 18:53:44 +0100

    MY forth commit

--- app/car.js
+++ app/car.js
@@ -1,10 +1,32 @@
-module.exports{
-	class Car{
-		constructor(name,model,type){
-			this.name=name;
-			this.model=model;
-			this.type=type;
+module.exports = function(name, model, type){
+      this.name= name || 'General';
+      this.model= model || 'GM';
+      this.type= type;
+      this.numOfDoors= 4;
+      this.numOfWheels= 4;
+      this.speed;
+      this.isSaloon;
 
-		}
-	}
-}
+      if(name === "Porshe" || name==="Koenigsegg"){
+          this.numOfDoors= 2;
+      }
+      if(type === "trailer"){
+          this.numOfWheels = 8;
+          this.speed= "0 km/h";
+      }
+      if(this.numOfWheels === 4){
+          this.isSaloon = true;
+      }
+      else{
+          this.isSaloon = false; 
+      }
+      this.drive= function(num){
+          if(this.type === 'trailer'){
+              this.speed = "77 km/h";
+          }
+          if(this.name === "Porshe"){
+              this.speed= "250 km/h";
+          }
+          return this;
+      }
+  }
